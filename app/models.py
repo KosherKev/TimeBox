6c824e646506eaed6ecde38df2d97c2d88b123b9
KosherKev <kevinafenyo@icloud.com>
2024-08-29 10:42:43 +0000

    List time periods

--- app/models.py
+++ app/models.py
@@ -1,11 +1,7 @@
-from sqlalchemy import Column, Integer, String, DateTime, Enum, TIMESTAMP, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Time, Enum, TIMESTAMP, ForeignKey, text
 from sqlalchemy.orm import relationship
 from .database import Base
 import enum
-
-class PriorityEnum(enum.Enum):
-    P = "P"
-    S = "S"
 
 class User(Base):
     __tablename__ = 'users'
@@ -15,14 +11,34 @@ class User(Base):
     user_password = Column(String(100), nullable=False)
     # tasks = relationship("Task", back_populates="user")
 
+class TaskAssignment(Base):
+    __tablename__ = 'task_assignments'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    task_id = Column(Integer, ForeignKey('tasks.task_id'))
+    task_period_id = Column(Integer, ForeignKey('time_periods.id'))
+    tasks = relationship("Task", back_populates="assignment")
+    time_period = relationship("TimePeriod", back_populates="assignment")
+
 class Task(Base):
     __tablename__ = 'tasks'
+
     task_id = Column(Integer, primary_key=True, autoincrement=True)
     task_name = Column(String(100), nullable=False)
-    task_description = Column(String(255))
-    startT = Column(DateTime)
-    endT = Column(DateTime)
-    priority = Column(Enum(PriorityEnum))
-    created_on = Column(TIMESTAMP)
+    task_description = Column(String(255), nullable=False)
+    priority = Column(Enum('P', 'S'), nullable=False)
+    created_on = Column(TIMESTAMP, server_default=text('CURRENT_TIMESTAMP'))
+    updated_on = Column(TIMESTAMP, server_default=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+    assignment_id = Column(Integer, ForeignKey('task_assignments.id'))
+    assignment = relationship("TaskAssignment", back_populates="task")
+
+class TimePeriod(Base):
+    __tablename__ = 'time_periods'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    start_time = Column(Time)
+    assignment_id = Column(Integer, ForeignKey('task_assignments.id'))
+    assignment = relationship("TaskAssignment", back_populates="time_period")
+
+    # startT = Column(DateTime)
+    # endT = Column(DateTime)
     # user_id = Column(Integer, ForeignKey('users.user_id'))
     # user = relationship("User", back_populates="tasks")
